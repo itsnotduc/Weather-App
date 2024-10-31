299381f3b849a928adc22c055e846cf5e11649db
Duc Nguyen <ndmd21103@gmail.com>
2024-10-31 17:25:44 +0200

    easter eggs, transition background

--- src/components/Rain.jsx
+++ src/components/Rain.jsx
@@ -1,21 +1,18 @@
-// src/components/Rain.jsx
-
 import React, { useEffect, useState } from 'react';
 import './Rain.css';
 
-export const createRaindrops = (isRaining) => {
+export const createRaindrops = (isRaining, isHeartRain) => {
     const drops = [];
-    const dropCount = 100; // Total number of raindrops
-
+    const dropCount = 100;
     for (let i = 0; i < dropCount; i++) {
         drops.push(
             <div
-                className="rain__drop"
+                className={`rain__drop ${isHeartRain ? 'heart__drop' : ''}`}
                 style={{
-                    left: `${Math.random() * 100}vw`, // Random horizontal position
-                    animationDuration: `${Math.random() * 0.5 + 0.5}s`, // Random fall speed
-                    animationDelay: `${Math.random() * 2}s`, // Random delay for each drop
-                    opacity: isRaining ? 0.7 : 0, // Change opacity based on raining state
+                    left: `${Math.random() * 100}vw`,
+                    animationDuration: `${isHeartRain ? Math.random() * 1.5 + 4 : Math.random() * 0.5 + 0.5}s`, // Corrected syntax
+                    animationDelay: `${Math.random() * 2}s`,
+                    opacity: isRaining ? 0.7 : 0,
                 }}
                 key={i}
             ></div>
@@ -24,24 +21,21 @@ export const createRaindrops = (isRaining) => {
     return drops;
 };
 
-const Rain = () => {
-    const [isRaining, setIsRaining] = useState(false); // Track if it's raining
-
-    // Use useEffect to set raining state
-    useEffect(() => {
-        // Set a timeout to start the rain after a brief moment
-        const timer = setTimeout(() => {
-            setIsRaining(true); // Start raining after a short delay
-        }, 500); // Adjust the delay as needed (500ms here)
-        
-        return () => clearTimeout(timer); // Clean up the timer on unmount
-    }, []);
-
+const Rain = ({ isRaining, isHeartRain }) => {
     return (
-        <div className="rain" style={{ position: 'fixed', top: 0, left: 0, right: 0, bottom: 0, zIndex: 0 }}>
-            {createRaindrops(isRaining)} {/* Pass isRaining state */}
+        <div className="rain" style={{
+            position: 'fixed',
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            zIndex: 0,
+            opacity: isRaining ? 1 : 0,
+            transition: 'opacity 2s ease-in-out'
+        }}>
+            {createRaindrops(isRaining, isHeartRain)}
         </div>
     );
 };
 
-export default React.memo(Rain); // Optimize with React.memo
+export default React.memo(Rain);
